2a924922e807e80caf1d5edc5c1bb6fabb5506fb
arfinaleusuf <arfinaleusuf@gmail.com>
2026-07-30 02:02:19 +0600

    delete

--- main.py
+++ main.py
@@ -107,3 +107,16 @@ def update_student(student_id: str, student: StudentUpdate):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message': 'Student Updated Successfully'})
+
+@app.delete("/delete/{student_id}")
+def delete_student(student_id: str):
+    data = load_data()
+
+    if student_id not in data:
+        raise HTTPException(status_code=404, detail="Student Not Found")
+
+    del data[student_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Student Deleted Successfully'})
